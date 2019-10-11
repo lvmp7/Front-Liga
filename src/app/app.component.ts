208cdf836f9e315040eb1cb056a111b9ee2c8a0d
lvmp7 <luiz.vmpereira@gmail.com>
2019-10-11 07:56:54 -0300

    Ajustes na tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {MenuComponent} from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontLiga';
+  title = 'Liga';
+  menu = MenuComponent;
+
+  getBackground(){
+      return 'url(\'./img/background.jpg\')';
+  }
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+
+const appRoutes: Routes = [
+  { path: 'menu', component: MenuComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
